9b7fb6e53758b4bcf56d8c37cfe365b570f51758
Will Robinson <william.ellis.robinson@gmail.com>
2023-11-25 22:36:20 +0000

    feat: added .gitignore, git config executable, basic first pre-commit hook

--- run.sh
+++ run.sh
@@ -23,3 +23,27 @@ fi
 
 echo "Creating .githooks/ directory at target git directory: $path"
 mkdir -p "$path/.githooks"
+
+echo "Adding .githooks/ directory to .gitignore"
+if [[ ! -e "$path/.gitignore" ]]; then
+    echo "Creating .gitignore file"
+    touch "$path/.gitignore"
+fi
+gitignore_file="$path/.gitignore"
+echo ".githooks/" >> "$gitignore_file"
+
+cd "$path" || exit 1
+
+echo "Running: git config --local core.hooksPath .githooks/"
+git config --local core.hooksPath .githooks/
+
+echo "Creating pre-commit hook"
+pre_commit_script="$path/.githooks/pre-commit"
+
+echo "#!/bin/bash" > "$pre_commit_script"
+echo "" >> "$pre_commit_script"
+echo "echo \"Running pre-commit hook\"" >> "$pre_commit_script"
+
+chmod +x "$pre_commit_script"
+
+echo "Git hook configurations completed for the repository at: $path"
